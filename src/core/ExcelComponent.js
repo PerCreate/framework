7ba4f49a540232577063bcea2cc008f6a1429f99
Kostya <kostya.pershin007@gmail.com>
2021-12-10 18:38:25 +0400

    add продолжаю разработку!!!

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,17 @@
+import { DomListener } from "./DomListener";
+
+export class ExcelComponent extends DomListener {
+	constructor($root, options = {}) {
+		super($root, options.listeners);
+
+	}
+
+	// Возвращает шаблон компонента
+	toHTML() {
+		return '';
+	}
+
+	init() {
+		this.initDomListeners();
+	}
+}
